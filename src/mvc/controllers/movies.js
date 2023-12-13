70f30f0f06d8f6dcbc07d44969ba97d286ac596f
Emanuel Peire <emapeire@outlook.com>
2023-12-13 20:54:09 -0300

    Update dependencies and file paths

--- src/mvc/controllers/movies.js
+++ src/mvc/controllers/movies.js
@@ -1,4 +1,4 @@
-import { MovieModel } from '../models/movie.js'
+import { MovieModel } from '../models/local/movie.js'
 import { validateMovie, validatePartialMovie } from '../../schemas/index.js'
 
 export class MovieController {
