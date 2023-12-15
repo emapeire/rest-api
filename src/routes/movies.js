7bfabb79f740d34f09ec637ecebc07fcdbb81f9d
Emanuel Peire <emapeire@outlook.com>
2023-12-15 10:32:50 -0300

    Update import path for MovieController

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { MovieController } from '../controllers/movies.js'
+import { MovieController } from '../mvc/controllers/movies.js'
 
 export const moviesRouter = Router()
 
